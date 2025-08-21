3f6cf4373a7e296daf92641316974b83420dd7fa
Vincent Livant <134003297+vlivant-volpe@users.noreply.github.com>
2025-08-21 09:38:22 -0400

    Updated gitignore

---
+++ CAFE_Comment_Pulling_Tool/src/_scalars.R
@@ -0,0 +1,7 @@
+# This file acts as a template for the required file "_scalars.R"
+# The Comment Pulling Tool uses "_scalars.R" to define the below variables
+
+docketId <- # e.g. "EPA-HQ-OAR-2022-0985"
+
+indexId <- # e.g. "2024-06-10"
+lastVersion <- # Use "none" to start from scratch
